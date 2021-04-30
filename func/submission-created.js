9e5094aebfedc1a6d362b9224364088e8230395a
Aida Enna <Aida-Enna@users.noreply.github.com>
2021-04-30 12:22:57 -0400

    no unban link

--- func/submission-created.js
+++ func/submission-created.js
@@ -67,15 +67,6 @@ exports.handler = async function (event, context) {
             } catch (e) {
                 console.log(e);
             }
-
-            if (!process.env.DISABLE_UNBAN_LINK) {
-                const unbanUrl = new URL("/.netlify/functions/unban", process.env.URL);
-                const unbanInfo = {
-                    userId: userInfo.id
-                };
-    
-                message.embed.description = `[Approve appeal and unban user](${unbanUrl.toString()}?token=${encodeURIComponent(createJwt(unbanInfo))})`;
-            }
         }
 
         const result = await fetch(`${API_ENDPOINT}/channels/${encodeURIComponent(process.env.APPEALS_CHANNEL)}/messages`, {
